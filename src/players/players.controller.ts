e829c75d2c42aa477a5a0fa0d5e3477ac336d943
Crystopher Carvalho <43764340+Zackynson@users.noreply.github.com>
2022-07-02 18:12:33 -0300

    feat: add update player controller and service

--- src/players/players.controller.ts
+++ src/players/players.controller.ts
@@ -1,8 +1,11 @@
-import { Body, Controller, Get, Post } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post, Put } from '@nestjs/common';
 import { CreatePlayerDTO } from './dtos/create-player-dto';
 import { Player } from './entities/Player';
 import { PlayersService } from './players.service';
 import { CreatePlayerValidationPipe } from './pipes/create-player-validation.pipe';
+import { UpdatePlayerDTO } from './dtos/update-player-dto';
+import { UpdatePlayerDataValidationPipe } from './pipes/update-player-data-validation.pipe';
+import { PlayerIdParamValidationPipe } from './pipes/player-id-param-validation.pipe';
 
 @Controller('players')
 export class PlayersController {
@@ -13,6 +16,15 @@ export class PlayersController {
     @Body(CreatePlayerValidationPipe) createPlayerDTO: CreatePlayerDTO,
   ): Promise<void> {
     await this.playersService.create(createPlayerDTO);
+  }
+
+  @Put(':id')
+  async updatePlayer(
+    @Param('id', PlayerIdParamValidationPipe) id: string,
+    @Body(UpdatePlayerDataValidationPipe) updatePlayerDTO: UpdatePlayerDTO,
+  ): Promise<void> {
+    console.log(id);
+    await this.playersService.update({ id, data: updatePlayerDTO });
   }
 
   @Get()
--- src/players/players.service.ts
+++ src/players/players.service.ts
@@ -1,8 +1,14 @@
-import { Injectable, Logger } from '@nestjs/common';
+import { Injectable, Logger, NotFoundException } from '@nestjs/common';
 import { v4 } from 'uuid';
 
 import { CreatePlayerDTO } from './dtos/create-player-dto';
+import { UpdatePlayerDTO } from './dtos/update-player-dto';
 import { Player } from './entities/Player';
+
+type UpdateParams = {
+  id: string;
+  data: UpdatePlayerDTO;
+};
 
 @Injectable()
 export class PlayersService {
@@ -20,6 +26,27 @@ export class PlayersService {
     new Logger(PlayersService.name).log({ player });
 
     this.players.push(player);
+  }
+
+  async update({ id, data }: UpdateParams): Promise<void> {
+    const foundPlayerIndex = this.players.findIndex((p) => p.id === id);
+
+    if (foundPlayerIndex < 0) {
+      throw new NotFoundException('Player not found');
+    }
+
+    new Logger(PlayersService.name).log({
+      originalPlayer: this.players[foundPlayerIndex],
+    });
+
+    this.players[foundPlayerIndex] = {
+      ...this.players[foundPlayerIndex],
+      ...data,
+    };
+
+    new Logger(PlayersService.name).log({
+      updatedPlayer: this.players[foundPlayerIndex],
+    });
   }
 
   async find(): Promise<Player[]> {
