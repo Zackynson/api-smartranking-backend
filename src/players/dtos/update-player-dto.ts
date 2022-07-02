e509e0f42b0acfffbe7577ceeeb5839a05110399
Crystopher Carvalho <43764340+Zackynson@users.noreply.github.com>
2022-07-02 18:12:10 -0300

    feat: add update player dto

---
+++ src/players/dtos/update-player-dto.ts
@@ -0,0 +1,7 @@
+export class UpdatePlayerDTO {
+  readonly name?: string;
+  readonly phoneNumber?: string;
+  readonly avatarUrl?: string;
+  readonly ranking?: string;
+  readonly rankingPosition?: number;
+}
